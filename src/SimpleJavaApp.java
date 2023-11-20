c9ba97bb9de9f0d1fe6edfe98d168a85e0f2102a
Amir Dirin <amir.dirin@yahoo.com>
2023-11-20 23:50:47 +0200

    yml is added

--- src/SimpleJavaApp.java
+++ src/SimpleJavaApp.java
@@ -1,5 +1,7 @@
 public class SimpleJavaApp {
     public static void main(String[] args) {
+        // this is a new print
         System.out.println("Hello, DevOps!");
+        System.out.println("Not much");
     }
 }
